67b665ab84990475d4234ac9a83a8f7afc89cabe
ecthon <ecthon@gmail.com>
2022-11-17 18:02:48 -0300

    Requisições HTTP - #18

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -7,16 +7,13 @@ import { ListService } from 'src/app/services/list.service';
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent implements OnInit {
-  animals: IAnimal[] = [
-    { name: "Tuca", type: "Dog", age: 2 },
-    { name: "Chulepa", type: "Dog", age: 3 },
-    { name: "Pituca", type: "Cat", age: 1 },
-    { name: "Kito", type: "Ave", age: 1 }
-  ]
+  animals: IAnimal[] = []
 
   animalDetails = ''
 
-  constructor(private listService: ListService) { }
+  constructor(private listService: ListService) {
+    this.getAnimals()
+  }
 
   ngOnInit(): void {
   }
@@ -28,6 +25,10 @@ export class ListRenderComponent implements OnInit {
   removeAnimal(animal: IAnimal) {
     console.log('Removendo animal ...')
     this.animals = this.listService.remove(this.animals, animal);
+  }
+
+  getAnimals(): void {
+    this.listService.getAll().subscribe((animals) => (this.animals = animals));
   }
 
 }
--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { IAnimal } from '../Animal';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ListService {
+  private apiUrl = 'http://localhost:3000/animals'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   remove(animals: IAnimal[], animal: IAnimal) {
     return animals.filter((a) => animal.name !== a.name);
   }
+
+  getAll(): Observable<IAnimal[]> {
+    return this.http.get<IAnimal[]>(this.apiUrl)
+  }
+
 }
